406af59ad790cc318d1ae4e92037334c83b0977a
amina <aminabakkali2001@gmail.com>
2023-12-29 17:33:47 +0100

    ms3 init

---
+++ src/main/java/ma/fstt/microservice3/Microservice3Application.java
@@ -0,0 +1,17 @@
+package ma.fstt.microservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("ma.fstt.microservice3.controller")
+@EnableDiscoveryClient
+public class Microservice3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice3Application.class, args);
+    }
+
+}
---
+++ src/main/java/ma/fstt/microservice3/controller/MessageRestController.java
@@ -0,0 +1,24 @@
+package ma.fstt.microservice3.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(method = {RequestMethod.POST,RequestMethod.GET} , path = "/service3")
+
+public class MessageRestController {
+
+
+    @GetMapping("/sayhello/{message}")
+    public String SayHello(@PathVariable String message){
+
+        return "Hello : "+ message;
+    }
+
+    @GetMapping("/{msg}")
+    public String Service3(@PathVariable String msg){
+
+        return "Microservice 3 : "+ msg;
+    }
+
+}
